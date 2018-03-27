8f351bc2a0e095fc977e8dac26a8e20cb6643e00
Mario Meissner <mr.rash.boy@gmail.com>
2018-03-27 17:39:16 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# calculator
+
+A calculator app for IOS and Android built using the Flutter framework.
+
+
---
+++ lib/pages/calc_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Extend the StatelessWidget to create our own Homepage widget
+class CalcHome extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+    // A 'material' is like a canvas where we can put things on
+    return new Material(
+      color: Colors.blue,
+    );
+  }
+}
