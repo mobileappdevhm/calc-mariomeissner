a04e15ff64ba878b35ee005d3e9d8e1171b6b318
Mario Meissner <mr.rash.boy@gmail.com>
2018-03-30 14:57:16 +0200

    Add tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,23 +7,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculator/main.dart';
+import 'package:calculator/calculator.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new Calculator());
+
+    // Check if buttons are on screen
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('x'), findsOneWidget);
+    expect(find.text('A'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.widgetWithText(NumberKey, '2'));
+    await tester.tap(find.widgetWithText(OperatorKey, 'x'));
+    await tester.tap(find.widgetWithText(NumberKey, '3'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // Verify that the operation was successful
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('6'), findsOneWidget);
   });
 }
